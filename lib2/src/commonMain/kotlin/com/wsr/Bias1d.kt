393e0a8ece1f45efdb5aa54b7ec9df80381e2fb2
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-28 01:09:31 +0900

    [add] Bias1dを追加

---
+++ lib2/src/commonMain/kotlin/com/wsr/Bias1d.kt
@@ -0,0 +1,20 @@
+package com.wsr
+
+import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.api.ndarray
+import org.jetbrains.kotlinx.multik.ndarray.data.D1
+import org.jetbrains.kotlinx.multik.ndarray.data.D1Array
+import org.jetbrains.kotlinx.multik.ndarray.operations.minus
+import org.jetbrains.kotlinx.multik.ndarray.operations.plus
+
+class Bias1d(num: Int) : Layer<D1> {
+    private var weight: D1Array<Double> = mk.ndarray(List(num) { 0.0 })
+    override fun forward(input: D1Array<Double>): D1Array<Double> {
+        return input + weight
+    }
+
+    override fun backward(delta: D1Array<Double>): D1Array<Double> {
+        weight = weight - delta
+        return delta
+    }
+}
