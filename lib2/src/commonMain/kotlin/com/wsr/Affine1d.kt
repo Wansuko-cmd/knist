dccf686e158b7fab5a66704af17c5b86a5c15984
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-28 08:47:29 +0900

    [fix] Affine1dを置き換え

--- lib2/src/commonMain/kotlin/com/wsr/Affine1d.kt
+++ lib2/src/commonMain/kotlin/com/wsr/Affine1d.kt
@@ -1,27 +1,43 @@
 package com.wsr
 
-import org.jetbrains.kotlinx.multik.api.linalg.dot
-import org.jetbrains.kotlinx.multik.api.mk
-import org.jetbrains.kotlinx.multik.api.ndarray
-import org.jetbrains.kotlinx.multik.ndarray.data.D1
-import org.jetbrains.kotlinx.multik.ndarray.data.D1Array
-import org.jetbrains.kotlinx.multik.ndarray.data.D2Array
-import org.jetbrains.kotlinx.multik.ndarray.data.set
+import com.wsr.common.IOType1d
+import com.wsr.common.IOType2d
 
 class Affine1d(
-    numOfInput: Int,
+    private val numOfInput: Int,
     private val numOfNeuron: Int,
-) : Layer<D1> {
-    private var weight: D2Array<Double> = mk.ndarray(List(numOfInput) { List(numOfNeuron) { 0.0 } })
-    private var input: D1Array<Double>? = null
-    override fun forward(input: D1Array<Double>): D1Array<Double> {
-        this.input = input
-        return weight.dot(input)
+) : Layer2<IOType1d> {
+    private val weight: IOType2d = Array(numOfInput) { Array(numOfNeuron) { 0.0 } }
+    override fun expect(input: IOType1d): IOType1d = forward(input)
+
+    override fun train(input: IOType1d, delta: (IOType1d) -> IOType1d): IOType1d {
+        val output = forward(input)
+        val delta = delta(output)
+        for (inputIndex in 0..numOfInput) {
+            for (outputIndex in 0..numOfNeuron) {
+                weight[inputIndex][outputIndex] = delta[outputIndex] * input[inputIndex]
+            }
+        }
+        val before = Array(numOfInput) { 0.0 }
+        for (inputIndex in 0..numOfInput) {
+            var sum = 0.0
+            for (outputIndex in 0..numOfNeuron) {
+                sum += delta[outputIndex] * weight[inputIndex][outputIndex]
+            }
+            before[inputIndex] = sum
+        }
+        return before
     }
 
-    override fun backward(delta: D1Array<Double>): D1Array<Double> {
-        delta.dot(input!!.transpose(0))
-        weight.transpose(0, 1).dot(delta)
-        TODO()
+    private fun forward(input: IOType1d): IOType1d {
+        val output = Array(numOfNeuron) { 0.0 }
+        for (outputIndex in 0..numOfNeuron) {
+            var sum = 0.0
+            for (inputIndex in 0..numOfInput) {
+                sum += input[inputIndex] * weight[inputIndex][outputIndex]
+            }
+            output[outputIndex] = sum
+        }
+        return output
     }
 }
