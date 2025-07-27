1c1f9cb1913195062f2e34d16df582d93357bc1b
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-28 00:39:52 +0900

    [add] multikへの依存を追加

--- lib2/build.gradle.kts
+++ lib2/build.gradle.kts
@@ -10,6 +10,8 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(libs.coroutine)
+                implementation("org.jetbrains.kotlinx:multik-core:0.2.3")
+                implementation("org.jetbrains.kotlinx:multik-default:0.2.3")
             }
         }
 
