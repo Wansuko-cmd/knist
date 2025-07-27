d837ed44165b4aaad494ef8370e831caca93d2dc
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-28 08:26:11 +0900

    [fix] Bias1dを置き換え

--- lib2/src/commonMain/kotlin/com/wsr/Bias1d.kt
+++ lib2/src/commonMain/kotlin/com/wsr/Bias1d.kt
@@ -1,5 +1,6 @@
 package com.wsr
 
+import com.wsr.common.IOType1d
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.ndarray
 import org.jetbrains.kotlinx.multik.ndarray.data.D1
@@ -7,14 +8,26 @@ import org.jetbrains.kotlinx.multik.ndarray.data.D1Array
 import org.jetbrains.kotlinx.multik.ndarray.operations.minus
 import org.jetbrains.kotlinx.multik.ndarray.operations.plus
 
-class Bias1d(numOfInput: Int) : Layer<D1> {
-    private var weight: D1Array<Double> = mk.ndarray(List(numOfInput) { 0.0 })
-    override fun forward(input: D1Array<Double>): D1Array<Double> {
-        return input + weight
+class Bias1d(numOfInput: Int) : Layer2<IOType1d> {
+    private val weight = Array(numOfInput) { 0.0 }
+    override fun expect(input: IOType1d): IOType1d {
+        for (i in input.indices) {
+            input[i] += weight[i]
+        }
+        return input
     }
 
-    override fun backward(delta: D1Array<Double>): D1Array<Double> {
-        weight = weight - delta
+    override fun train(
+        input: IOType1d,
+        delta: (IOType1d) -> IOType1d,
+    ): IOType1d {
+        for (i in input.indices) {
+            input[i] += weight[i]
+        }
+        val delta = delta(input)
+        for (i in weight.indices) {
+            weight[i] -= delta[i]
+        }
         return delta
     }
 }
