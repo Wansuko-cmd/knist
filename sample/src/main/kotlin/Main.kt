e1ea63ba850a16232edf01a1c8ea3d0597ad389f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-29 08:57:23 +0900

    wip

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -47,40 +47,4 @@ private fun createIrisModel2(
             ),
         ).toList().maxIndex() == data.label
     }.let { println(it.toDouble() / test.size.toDouble()) }
-
-
-//    val network = Network.create0d(
-//        Input0dLayer(4),
-//        listOf(
-//            Affine(50, ::relu),
-//            Bias0d(::relu),
-//        ),
-//        Softmax0d(3) { numOfNeuron, activationFunction -> Affine(numOfNeuron, activationFunction) },
-//        random = seed?.let { Random(it) } ?: Random,
-//        rate = 0.01,
-//    )
-//    (1..epoc).forEach { epoc ->
-////        println("epoc: $epoc")
-//        train.forEach { data ->
-//            network.train(
-//                input = listOf(
-//                    data.petalLength,
-//                    data.petalWidth,
-//                    data.sepalLength,
-//                    data.sepalWidth,
-//                ),
-//                label = data.label,
-//            )
-//        }
-//    }
-//    test.count { data ->
-//        network.expect(
-//            input = listOf(
-//                data.petalLength,
-//                data.petalWidth,
-//                data.sepalLength,
-//                data.sepalWidth,
-//            ),
-//        ) == data.label
-//    }.let { println(it.toDouble() / test.size.toDouble()) }
 }
