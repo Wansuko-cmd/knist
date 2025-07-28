e6a416a0473350a10cfc2e9afeca7145fe74a04f
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-29 00:57:09 +0900

    wip

--- lib2/src/commonMain/kotlin/com/wsr/Network.kt
+++ lib2/src/commonMain/kotlin/com/wsr/Network.kt
@@ -3,9 +3,6 @@ package com.wsr
 import com.wsr.common.IOType1d
 
 class Network2(private val layers: List<Layer<IOType1d>>) {
-    fun expect(input: IOType1d): IOType1d =
-        layers.fold(input) { acc, layer -> layer.expect(acc) }
-
     private val trainLambda: (IOType1d, IOType1d) -> IOType1d = layers
         .reversed()
         .fold(::output) { acc: (IOType1d, IOType1d) -> IOType1d, layer: Layer<IOType1d> ->
@@ -16,6 +13,9 @@ class Network2(private val layers: List<Layer<IOType1d>>) {
 
     private fun output(input: IOType1d, label: IOType1d) =
         Array(input.size) { input[it] - label[it] }
+
+    fun expect(input: IOType1d): IOType1d =
+        layers.fold(input) { acc, layer -> layer.expect(acc) }
 
     fun train(input: IOType1d, label: IOType1d) {
         trainLambda(input, label)
