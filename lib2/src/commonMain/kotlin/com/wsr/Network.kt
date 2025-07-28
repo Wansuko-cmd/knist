b0904b2a4e93920b94ccbc3b3f6fb334fcdd48b5
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-07-29 00:56:09 +0900

    wip

--- lib2/src/commonMain/kotlin/com/wsr/Network.kt
+++ lib2/src/commonMain/kotlin/com/wsr/Network.kt
@@ -6,24 +6,18 @@ class Network2(private val layers: List<Layer<IOType1d>>) {
     fun expect(input: IOType1d): IOType1d =
         layers.fold(input) { acc, layer -> layer.expect(acc) }
 
-    fun train(input: IOType1d, label: IOType1d) {
-//        layers[0].train(input) { o0 ->
-//            layers[1].train(o0) { o1 ->
-//                layers[2].train(o1) { o2 ->
-//                    layers[3].train(o2) { o3 ->
-//                        layers[4].train(o3) { o4 ->
-//                            layers[5].train(o4) { o5 ->
-//                                Array(o5.size) { o5[it] - label[it] } }
-//                            }
-//                        }
-//                }
-//            }
-//        }
-        val deltaList = mutableListOf({ input: IOType1d -> Array(input.size) { input[it] - label[it] } })
-        for (index in layers.lastIndex downTo 0) {
-            val i = deltaList.lastIndex
-            deltaList.add { input: IOType1d -> layers[index].train(input, deltaList[i]) }
+    private val trainLambda: (IOType1d, IOType1d) -> IOType1d = layers
+        .reversed()
+        .fold(::output) { acc: (IOType1d, IOType1d) -> IOType1d, layer: Layer<IOType1d> ->
+            { input: IOType1d, label: IOType1d ->
+                layer.train(input) { acc(it, label) }
+            }
         }
-        deltaList.last().invoke(input)
+
+    private fun output(input: IOType1d, label: IOType1d) =
+        Array(input.size) { input[it] - label[it] }
+
+    fun train(input: IOType1d, label: IOType1d) {
+        trainLambda(input, label)
     }
 }
